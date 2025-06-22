b63349b99f5417977eab3ca9c85717af2d969c71
jvxiao <2805431021@qq.com>
2025-06-22 21:59:03 +0800

    feat: 调整站点布局

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {resolve} from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/image-compression/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, './public/index.html'),
+        'image-compression': resolve(__dirname, 'compression.html'),
+      }
+    }
+  }
 })
