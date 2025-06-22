baa83ba485c783e3bc626cce956fabcbaa0750c7
jvxiao <2805431021@qq.com>
2025-06-22 22:49:32 +0800

    chore: 流水线构建报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from 'path';
+import {resolve, dirname} from 'path';
+import { fileURLToPath } from 'node:url'
 // https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   // base: '/image-compression/',
   plugins: [vue()],
